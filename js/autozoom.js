06d6842d5f61a846091972728474962e2c697085
ecmascriptguru <ecmascript.guru@gmail.com>
2017-06-03 15:42:58 -0400

    skeleton created.

---
+++ js/autozoom.js
@@ -0,0 +1,34 @@
+'use strict';
+let AutoZoom = (function() {
+	let _data = [],
+		something = null;
+
+	let capture = () => {
+		let first_name = ($("input#app_first_name")[0] || {}).value;
+		alert(first_name);
+	}
+
+	let init = () => {
+		chrome.runtime.onMessage.addListener((request, sender, sendMessage) => {
+			switch(request.from) {
+				case "popup":
+					if (request.action == "capture") {
+						capture();
+					}
+					break;
+
+				default:
+					console.log("Unknown msg..");
+					break;
+			}
+		});
+	};
+		
+	return {
+		init: init
+	};
+})();
+
+(function(window, jQuery) {
+	AutoZoom.init();
+})(window, $);
